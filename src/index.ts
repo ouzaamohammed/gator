2c1a860940a5468e359045f0c0a0ec0be4480497
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-12-06 16:01:59 +0100

    implement setting the current user name

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import { readConfig, setUser } from "./config";
+
 function main() {
-  console.log("Hello, world!");
+  setUser("lane");
+  const config = readConfig();
+  console.log(config);
 }
 
 main();
