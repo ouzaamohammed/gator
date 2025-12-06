5edc07e0b6e5282fcc1f976818f8c789adaf0008
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-12-06 18:03:12 +0100

    add a command registry and login command

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-import { readConfig, setUser } from "./config";
+import { registerCommand, runCommand, type CommandsRegistry } from "./commands";
+import { handlerLogin } from "./handlers/login";
 
 function main() {
-  setUser("lane");
-  const config = readConfig();
-  console.log(config);
+  const args = process.argv.slice(2);
+  if (args.length < 1) {
+    console.log("Usage: cli <command> [args...]");
+    process.exit(1);
+  }
+
+  const cmdName = args[0];
+  const cmdArgs = args.slice(1);
+  const registry: CommandsRegistry = {};
+
+  registerCommand(registry, "login", handlerLogin);
+
+  try {
+    runCommand(registry, cmdName, ...cmdArgs);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(`Error running command ${cmdName}: ${err.message}`);
+    } else {
+      console.error(`Error running command ${cmdName}: ${err}`);
+    }
+    process.exit(1);
+  }
 }
 
 main();
